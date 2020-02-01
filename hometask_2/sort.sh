a77717b008d5aedb663ad1b9b2929c861d0983dd
ivan <ivankob.16@gmail.com>
2020-02-01 21:12:31 +0200

    sort redone

--- hometask_2/sort.sh
+++ hometask_2/sort.sh
@@ -4,4 +4,6 @@ if [ ! -f "versions.txt" ]; then
   curl http://yoko.ukrtux.com:8899/versions.txt --output versions.txt
 fi
 
-sort -V versions.txt | uniq 
+sort -V -o versions.txt versions.txt
+
+cat versions.txt
