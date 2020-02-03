72f6dedf34891ca5d03742fcd652fde9973b16d0
ivan <ivankob.16@gmail.com>
2020-02-03 20:23:16 +0200

    improved update script

--- hometask_2/auto_update.sh
+++ hometask_2/auto_update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-log_dir=$(date +'%Y%m%d')
 sudo crontab -l > mycron
-echo "0 0 * * 0 (apt-get -y update && apt-get -y upgrade) >> /var/log/update-$log_dir.log 2>&1" >> mycron
+echo "0 0 * * 0 (apt-get -y update && apt-get -y upgrade) >> /var/log/update-$(date +'%Y%m%d').log 2>&1" >> mycron
 sudo crontab mycron
 rm mycron
