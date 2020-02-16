4da2c8903f223a82c4f97becc3fad7e18eec79e4
ivan <ivankob.16@gmail.com>
2020-02-16 12:15:31 +0200

    works_without_enc

--- hometask_4/prov_scr.sh
+++ hometask_4/prov_scr.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+sudo apt-get update -y
+echo “Installing and setting up Docker…”
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce
+sudo apt install docker.io -y
+echo "Configuring user…"
+sudo usermod -aG docker ubuntu
+sudo apt-get update -y
+echo "Completed provisioning"
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 
+sudo service docker restart 
 docker-compose -f /vagrant/docker-compose.yml up
